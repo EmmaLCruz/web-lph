d0db6930f3b57a3a19271939685bff1cd7b97a14
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-10-02 09:51:10 -0300

    Sección Testimonios terminada

--- carousels.js
+++ carousels.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   $(".testimonials__carousel").slick({
-    dots: true,
     infinite: true,
-    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 5000,
+    dots: true,
+    speed: 500,
     arrows: false,
   });
 });
